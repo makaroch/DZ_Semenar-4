00c78a2f9bd2ed9f8c82c28fc85eb28ca678c965
Alex <da.ek.ned@yandex.ru>
2022-09-28 14:34:25 +0300

    дз + отрезки

---
+++ pr_4/Program.cs
@@ -0,0 +1,76 @@
+﻿/*У нас есть график, где даны отрезок 1 - (х1; x2) и отрезок 2 (х3, x4)
+​Необходимо составить код, который находит их пересечение.
+Пересечением двух отрезка 1 и 2 могут быть - отрезок, точка или ничего
+С клавиатуры вводим х1, x2, х3, x4 и выводим ответ - границы пересечений отрезков, 
+общую точку или «не пересекаются».*/
+
+
+
+int[] creatArray(int x, int y){// функц. для заполнения масива по точкам
+    int[] arr = new int[Math.Abs(x-y)+1];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (x<y) { // заполняем масив от меньшего значения к большему
+            arr[i] = x;
+            x++;
+        }
+        else arr[i] = y;
+             y++;
+        }
+    return arr;
+}
+void lookArr(int[] arr){ // ввывести массив в консоль
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i]+", ");
+    }
+}
+
+void compareArr(int[] arr1, int[] arr2){ // веселье, сравниваю 2 массива если есть совпадения выводм в консоль
+    int cunt = 0;
+    if (arr1.Length > arr2.Length) {// первый for для более длинного мас. чтобы пройти все элементы
+        for (int i = 0; i < arr1.Length; i++)
+    {
+        for (int j = 0; j < arr2.Length; j++)
+        {
+            if (arr1[i] == arr2[j]){
+                Console.Write("точка пересеченя "+arr1[i]+" ");
+                cunt++;
+            }
+        }
+    }
+    } else {
+        for (int i = 0; i < arr2.Length; i++)
+    {
+        for (int j = 0; j < arr1.Length; j++)
+        {
+            if (arr2[i] == arr1[j]){
+                Console.Write("точка пересеченя "+arr2[i]+" ");
+                cunt++;
+            }
+        }
+    }
+    }
+    if (cunt == 0) Console.WriteLine("нет пересечений");
+    
+}
+
+
+
+int x1 = new Random().Next(-100,100);
+Console.WriteLine("x1 = "+x1);
+int x2 = new Random().Next(-100,100);
+Console.WriteLine("x2 = "+x2);
+int x3 = new Random().Next(-100,100);
+int x4 = new Random().Next(-100,100);
+
+int[] array1 = creatArray(x1,x2); // заполняем массивы (получаем наши отрезки)
+int[] array2 = creatArray(x3,x4);
+lookArr(array1);
+Console.WriteLine("");
+
+Console.WriteLine("x3 = "+x3);
+Console.WriteLine("x4 = "+x4);
+lookArr(array2);
+
+compareArr(array1,array2); // сравниваем 2 массива на пересечение
---
+++ task_25/Program.cs
@@ -0,0 +1,58 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+/*
+Console.WriteLine("введи 1 число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введи 2 число");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = a;
+for (int i = 1; i < b; i++)
+{
+    c = c * a;
+}
+Console.WriteLine(c);
+*/
+
+
+
+
+/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму
+цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+/*
+Console.WriteLine("введи число");
+string a = Console.ReadLine();
+int sum = 0;
+for (int i = 0; i < a.Length; i++)
+{// проверка на цифру 
+    if (a[i] == '1' | a[i] == '2' | a[i] == '3' | a[i] == '4' | a[i] == '5' | a[i] == '6' | a[i] == '7' | a[i] == '8' | a[i] == '9' | a[i] == '0'){
+        sum = sum + Convert.ToInt32(Convert.ToString(a[i]));
+    }
+}
+Console.WriteLine(sum);
+*/
+
+
+
+
+/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и
+выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+/*
+int[] arr = new int[8];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(0,10);
+    if (i != arr.Length - 1) Console.Write(arr[i] + ", ");
+    else Console.Write(arr[i] + ". ");
+}
+*/
